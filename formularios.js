ab7d33619a2c058a5ebed5ced2a540fe98ffdc2a
abivilleda28 <abigailfloresvilleda@gmail.com>
2024-11-05 01:20:10 -0600

    Subiendo mi tarea de Fer

--- formularios.js
+++ formularios.js
@@ -1,29 +1,28 @@
-document.getElementById("registrarse").addEventListener("click", function(){
+document.getElementById("Registrarse").addEventListener("click", function(event){
+    // Evita el comportamiento predeterminado del botón de enviar (enviar el formulario)
+    event.preventDefault();
+
+    // Recoger los valores de los campos del formulario
     const nombre = document.querySelector("#nombre input").value.trim();
     const email = document.querySelector("#email input").value.trim();
     const password = document.querySelector("#password input").value.trim();
     const tel = document.querySelector("#tel input").value.trim();
     const fecha = document.querySelector("#fecha input").value.trim();
 
-    if(nombre==="") {
-        alert("Porfavor ingresa tu nombre!");
-    
-    } else if (email==="") {
-        alert("Correo no aceptado");
-   
-    }else if (!email.includes("@")){
-        alert("No es un correo valido");
-   
-    }else if (password ===""){
-        alert ("Contraseña incorrecta");
-    
-    }else if (password.lenght < 6){
-        alert("La contraseña no es segura");
-   
-    }else if (fecha===""){
-        alert("No hay fecha de nacimiento");
-   
-    }else {
-        alert("Registro completo" + nombre + !);
+    // Validaciones
+    if (nombre === "") {
+        alert("¡Por favor ingresa tu nombre!");
+    } else if (email === "") {
+        alert("El correo no puede estar vacío.");
+    } else if (!email.includes("@")) {
+        alert("No es un correo válido.");
+    } else if (password === "") {
+        alert("¡Por favor ingresa una contraseña!");
+    } else if (password.length < 6) {
+        alert("La contraseña debe ser al menos de 6 caracteres.");
+    } else if (fecha === "") {
+        alert("¡Por favor ingresa tu fecha de nacimiento!");
+    } else {
+        alert("Registro completo, ¡bienvenido " + nombre + "!");
     }
 });
