6562b705b17ac50abcdba3e90615bc7b645f2064
abivilleda28 <abigailfloresvilleda@gmail.com>
2024-11-29 15:14:39 -0600

    añadiendo live coding 1

---
+++ live-coding-1/app.js
@@ -0,0 +1,25 @@
+// Escribe tu código aquí.
+
+function stringWords (word, array)  {
+    "Introduce un string"; 
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio', 'hola','goma','sacapuntas','limon'];
+ 
+stringWords('bocina', myArray);('filterButton')
+
+
+document.getElementById("filterButton").addEventListener('click', function() {
+        const inputWord = document.getElementById('inputWord').value;
+        onst myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+           
+const filteredWords = stringWords(word, array);
+
+printArray(filteredWords);  });
+  
+</body>
+
+document.getElementById
+
+
+console.log(filteredWords);
